9413a0ac3c33e8fc01905142c928ca652ae16cea
jhay-Ben <jamesbentil14@gmail.com>
2024-06-26 15:33:41 +0000

    Created the Sent, Receive, Loan, and Topup Component

---
+++ my-app/components/imagebutton.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity  } from 'react-native';
+
+const Buttons = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+            <TouchableOpacity>
+                <View style={styles.image}>
+                    <Image source={require('../assets/send.png')} />
+                </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                <View style={styles.image}>
+                    <Image source={require('../assets/recieve.png')} />
+                </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                <View style={styles.image}>
+                    <Image source={require('../assets/loan.png')} />
+                </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                <View style={styles.image}>
+                    <Image source={require('../assets/topUp.png')} />
+                </View>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.textContainer}>
+                <View style={styles.textBox}>
+                    <Text style={styles.text}>Sent</Text>
+                </View>
+                <View style={styles.textBox}>
+                    <Text style={styles.text}>Receive</Text>
+                </View>
+                <View style={styles.textBox}>
+                    <Text style={styles.text}>Loan</Text>
+                </View>
+                <View style={styles.textBox}>
+                    <Text style={styles.text}>Topup</Text>
+                </View>
+            </View>
+        </View>
+        
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1.9,
+        // flexDirection: 'row',
+        // marginTop: -190,
+        // padding: 25, 
+    },
+    imageContainer: {
+        // flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    image: {
+        backgroundColor: "#d1d1d1",
+        height: 70,
+        width: 70,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      textContainer: {
+        // flex: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+      },
+      textBox: {
+       
+      },
+      text: {
+        fontSize: 18
+      },
+});
+
+export default Buttons;
